045953c845a46bbd83f2dc7714023430d587288a
samb1232 <xah300@mail.ru>
2025-08-12 12:41:10 +0500

    added sleep for 1 sec

--- Core/Handlers/NewRecognitionHandler.cs
+++ Core/Handlers/NewRecognitionHandler.cs
@@ -4,6 +4,7 @@ public class NewRecognitionHandler
 {
     public void OnNewRecognition(object source, FileSystemEventArgs e)
     {
+        Thread.Sleep(1000);
         Console.WriteLine($"Changes detected in: {e.FullPath} ({e.ChangeType})");
     }
 }
