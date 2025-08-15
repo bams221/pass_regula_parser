e1ba569dd0e38e6a85bb97a2ed101a33c1efccb6
samb1232 <xah300@mail.ru>
2025-08-15 16:41:22 +0500

    added test for documentDataUpdater

---
+++ tests/Ui/DocumentDataUpdaterTest.cs
@@ -0,0 +1,25 @@
+using PassRegulaParser.Model;
+using System.Windows.Forms;
+using PassRegulaParser.Ui;
+
+namespace PassRegulaParser.Tests.Ui;
+
+public class DocumentDataUpdaterTests
+{
+    [Fact]
+    public void UpdateFromControls_ShouldUpdateDocumentProperties()
+    {
+        var passportData = new PassportData();
+        var updater = new DocumentDataUpdater(passportData);
+        var controls = new Dictionary<string, Control>
+        {
+            { "FullName", new TextBox { Text = "Иванов Иван Иванович" } },
+            { "SerialNumber", new TextBox { Text = "1234567890" } }
+        };
+
+        updater.UpdateFromControls(controls);
+
+        Assert.Equal("Иванов Иван Иванович", passportData.FullName);
+        Assert.Equal("1234567890", passportData.SerialNumber);
+    }
+}
