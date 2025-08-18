6628e0e6b51ca82521e6e88e12d53b733e720d38
samb1232 <xah300@mail.ru>
2025-08-18 16:35:29 +0500

    fix/changed style in tests

--- tests/Core/Utils/JsonFileParserTest.cs
+++ tests/Core/Utils/JsonFileParserTest.cs
@@ -1,11 +1,11 @@
 using PassRegulaParser.Core.Exceptions;
 using PassRegulaParser.Core.Utils;
 
-namespace PassRegulaParser.Tests.Core.Utils
+namespace PassRegulaParser.Tests.Core.Utils;
+
+public class JsonFileParserTests : IDisposable
 {
-    public class JsonFileParserTests : IDisposable
-    {
-        private const string TestJson = @"
+    private const string TestJson = @"
         {
             ""user"": {
                 ""name"": ""John Doe"",
@@ -20,153 +20,152 @@ namespace PassRegulaParser.Tests.Core.Utils
             ""emptyValue"": null
         }";
 
-        private readonly string _tempFilePath;
+    private readonly string _tempFilePath;
 
-        public JsonFileParserTests()
+    public JsonFileParserTests()
+    {
+        _tempFilePath = Path.GetTempFileName();
+        File.WriteAllText(_tempFilePath, TestJson);
+    }
+
+    public void Dispose()
+    {
+        if (File.Exists(_tempFilePath))
         {
-            _tempFilePath = Path.GetTempFileName();
-            File.WriteAllText(_tempFilePath, TestJson);
+            File.Delete(_tempFilePath);
         }
+    }
 
-        public void Dispose()
+    [Fact]
+    public void Constructor_WithValidFilePath_InitializesSuccessfully()
+    {
+        var exception = Record.Exception(() => new JsonFileParser(_tempFilePath));
+        Assert.Null(exception);
+    }
+
+    [Fact]
+    public void Constructor_WithEmptyFilePath_ThrowsParsingException()
+    {
+        Assert.Throws<ParsingException>(() => new JsonFileParser(""));
+    }
+
+    [Fact]
+    public void Constructor_WithNonExistentFile_ThrowsParsingException()
+    {
+        var nonExistentPath = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
+        Assert.Throws<ParsingException>(() => new JsonFileParser(nonExistentPath, 1));
+    }
+
+    [Fact]
+    public void Constructor_WithInvalidJson_ThrowsParsingException()
+    {
+        var invalidJsonPath = Path.GetTempFileName();
+        File.WriteAllText(invalidJsonPath, "{ invalid json }");
+
+        try
         {
-            if (File.Exists(_tempFilePath))
+            Assert.Throws<ParsingException>(() => new JsonFileParser(invalidJsonPath));
+        }
+        finally
+        {
+            File.Delete(invalidJsonPath);
+        }
+    }
+
+
+    [Fact]
+    public void Constructor_WithFileThatAppearsAfterRetry_Succeeds()
+    {
+        var delayedFilePath = Path.Combine(Path.GetTempPath(), $"delayed_{Guid.NewGuid()}.json");
+
+        try
+        {
+            Task.Run(async () =>
             {
-                File.Delete(_tempFilePath);
-            }
-        }
+                await Task.Delay(200);
+                File.WriteAllText(delayedFilePath, TestJson);
+            });
 
-        [Fact]
-        public void Constructor_WithValidFilePath_InitializesSuccessfully()
-        {
-            var exception = Record.Exception(() => new JsonFileParser(_tempFilePath));
+            var exception = Record.Exception(() => new JsonFileParser(delayedFilePath));
             Assert.Null(exception);
         }
-
-        [Fact]
-        public void Constructor_WithEmptyFilePath_ThrowsParsingException()
+        finally
         {
-            Assert.Throws<ParsingException>(() => new JsonFileParser(""));
-        }
-
-        [Fact]
-        public void Constructor_WithNonExistentFile_ThrowsParsingException()
-        {
-            var nonExistentPath = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
-            Assert.Throws<ParsingException>(() => new JsonFileParser(nonExistentPath, 1));
-        }
-
-        [Fact]
-        public void Constructor_WithInvalidJson_ThrowsParsingException()
-        {
-            var invalidJsonPath = Path.GetTempFileName();
-            File.WriteAllText(invalidJsonPath, "{ invalid json }");
-
-            try
+            if (File.Exists(delayedFilePath))
             {
-                Assert.Throws<ParsingException>(() => new JsonFileParser(invalidJsonPath));
-            }
-            finally
-            {
-                File.Delete(invalidJsonPath);
+                File.Delete(delayedFilePath);
             }
         }
+    }
+
+    [Fact]
+    public void Constructor_WithFileThatNeverAppears_ThrowsParsingException()
+    {
+        var nonExistentPath = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
+
+        var ex = Assert.Throws<ParsingException>(() => new JsonFileParser(nonExistentPath, 50));
+    }
 
 
-        [Fact]
-        public void Constructor_WithFileThatAppearsAfterRetry_Succeeds()
-        {
-            var delayedFilePath = Path.Combine(Path.GetTempPath(), $"delayed_{Guid.NewGuid()}.json");
+    [Fact]
+    public void GetStringProperty_WithValidPath_ReturnsCorrectValue()
+    {
+        var parser = new JsonFileParser(_tempFilePath);
+        var result = parser.GetStringProperty("user.name");
+        Assert.Equal("John Doe", result);
+    }
 
-            try
-            {
-                Task.Run(async () =>
-                {
-                    await Task.Delay(200);
-                    File.WriteAllText(delayedFilePath, TestJson);
-                });
+    [Fact]
+    public void GetStringProperty_WithNestedPath_ReturnsCorrectValue()
+    {
+        var parser = new JsonFileParser(_tempFilePath);
+        var result = parser.GetStringProperty("user.address.street");
+        Assert.Equal("123 Main St", result);
+    }
 
-                var exception = Record.Exception(() => new JsonFileParser(delayedFilePath));
-                Assert.Null(exception);
-            }
-            finally
-            {
-                if (File.Exists(delayedFilePath))
-                {
-                    File.Delete(delayedFilePath);
-                }
-            }
-        }
+    [Fact]
+    public void GetStringProperty_WithNonExistentPath_ThrowsParsingException()
+    {
+        var parser = new JsonFileParser(_tempFilePath);
+        Assert.Throws<ParsingException>(() => parser.GetStringProperty("user.nonexistent.property"));
+    }
 
-        [Fact]
-        public void Constructor_WithFileThatNeverAppears_ThrowsParsingException()
-        {
-            var nonExistentPath = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
+    [Fact]
+    public void GetStringProperty_WithNullValue_ThrowsParsingException()
+    {
+        var parser = new JsonFileParser(_tempFilePath);
+        Assert.Throws<ParsingException>(() => parser.GetStringProperty("emptyValue"));
+    }
 
-            var ex = Assert.Throws<ParsingException>(() => new JsonFileParser(nonExistentPath, 50));
-        }
+    [Fact]
+    public void GetStringProperty_WithNonStringValue_ThrowsParsingException()
+    {
+        var parser = new JsonFileParser(_tempFilePath);
+        Assert.Throws<ParsingException>(() => parser.GetStringProperty("user.age"));
+    }
 
+    [Fact]
+    public void GetNodeByPath_WithValidPath_ReturnsCorrectNode()
+    {
+        var parser = new JsonFileParser(_tempFilePath);
+        var node = parser.GetNodeByPath("user.address");
+        Assert.NotNull(node);
+        Assert.Equal("123 Main St", node["street"]?.GetValue<string>());
+    }
 
-        [Fact]
-        public void GetStringProperty_WithValidPath_ReturnsCorrectValue()
-        {
-            var parser = new JsonFileParser(_tempFilePath);
-            var result = parser.GetStringProperty("user.name");
-            Assert.Equal("John Doe", result);
-        }
+    [Fact]
+    public void GetNodeByPath_WithNonExistentPath_ReturnsNull()
+    {
+        var parser = new JsonFileParser(_tempFilePath);
+        Assert.Throws<ParsingException>(() => parser.GetNodeByPath("user.nonexistent.property"));
+    }
 
-        [Fact]
-        public void GetStringProperty_WithNestedPath_ReturnsCorrectValue()
-        {
-            var parser = new JsonFileParser(_tempFilePath);
-            var result = parser.GetStringProperty("user.address.street");
-            Assert.Equal("123 Main St", result);
-        }
-
-        [Fact]
-        public void GetStringProperty_WithNonExistentPath_ThrowsParsingException()
-        {
-            var parser = new JsonFileParser(_tempFilePath);
-            Assert.Throws<ParsingException>(() => parser.GetStringProperty("user.nonexistent.property"));
-        }
-
-        [Fact]
-        public void GetStringProperty_WithNullValue_ThrowsParsingException()
-        {
-            var parser = new JsonFileParser(_tempFilePath);
-            Assert.Throws<ParsingException>(() => parser.GetStringProperty("emptyValue"));
-        }
-
-        [Fact]
-        public void GetStringProperty_WithNonStringValue_ThrowsParsingException()
-        {
-            var parser = new JsonFileParser(_tempFilePath);
-            Assert.Throws<ParsingException>(() => parser.GetStringProperty("user.age"));
-        }
-
-        [Fact]
-        public void GetNodeByPath_WithValidPath_ReturnsCorrectNode()
-        {
-            var parser = new JsonFileParser(_tempFilePath);
-            var node = parser.GetNodeByPath("user.address");
-            Assert.NotNull(node);
-            Assert.Equal("123 Main St", node["street"]?.GetValue<string>());
-        }
-
-        [Fact]
-        public void GetNodeByPath_WithNonExistentPath_ReturnsNull()
-        {
-            var parser = new JsonFileParser(_tempFilePath);
-            Assert.Throws<ParsingException>(()=> parser.GetNodeByPath("user.nonexistent.property"));
-        }
-
-        [Fact]
-        public void Dispose_CanBeCalledMultipleTimes()
-        {
-            var parser = new JsonFileParser(_tempFilePath);
-            parser.Dispose();
-            var exception = Record.Exception(() => parser.Dispose());
-            Assert.Null(exception);
-        }
+    [Fact]
+    public void Dispose_CanBeCalledMultipleTimes()
+    {
+        var parser = new JsonFileParser(_tempFilePath);
+        parser.Dispose();
+        var exception = Record.Exception(() => parser.Dispose());
+        Assert.Null(exception);
     }
 }
--- tests/Core/Utils/PhotoUtilsTest.cs
+++ tests/Core/Utils/PhotoUtilsTest.cs
@@ -1,87 +1,83 @@
-using System;
-using System.IO;
-using Xunit;
 using PassRegulaParser.Core.Utils;
 
-namespace PassRegulaParser.Core.Tests.Utils
+namespace PassRegulaParser.Tests.Core.Utils;
+
+public class PhotoUtilsTests : IDisposable
 {
-    public class PhotoUtilsTests : IDisposable
+    private const string TestFilesDirectory = "TestFiles";
+    private const string ValidJpgFile = "test.jpg";
+    private const string ValidJpegFile = "test.jpeg";
+    private const string InvalidExtensionFile = "test.png";
+    private const string NonExistentFile = "nonexistent.jpg";
+
+    public PhotoUtilsTests()
     {
-        private const string TestFilesDirectory = "TestFiles";
-        private const string ValidJpgFile = "test.jpg";
-        private const string ValidJpegFile = "test.jpeg";
-        private const string InvalidExtensionFile = "test.png";
-        private const string NonExistentFile = "nonexistent.jpg";
+        Directory.CreateDirectory(TestFilesDirectory);
+    }
 
-        public PhotoUtilsTests()
+    public void Dispose()
+    {
+        if (Directory.Exists(TestFilesDirectory))
         {
-            Directory.CreateDirectory(TestFilesDirectory);
+            Directory.Delete(TestFilesDirectory, true);
         }
+    }
 
-        public void Dispose()
-        {
-            if (Directory.Exists(TestFilesDirectory))
-            {
-                Directory.Delete(TestFilesDirectory, true);
-            }
-        }
+    [Fact]
+    public void GetPhotoBase64FromFile_WhenFileDoesNotExist_ReturnsEmptyString()
+    {
+        var filePath = Path.Combine(TestFilesDirectory, NonExistentFile);
 
-        [Fact]
-        public void GetPhotoBase64FromFile_WhenFileDoesNotExist_ReturnsEmptyString()
-        {
-            var filePath = Path.Combine(TestFilesDirectory, NonExistentFile);
+        var result = PhotoUtils.GetPhotoBase64FromFile(filePath);
 
-            var result = PhotoUtils.GetPhotoBase64FromFile(filePath);
+        Assert.Equal(string.Empty, result);
+    }
 
-            Assert.Equal(string.Empty, result);
-        }
+    [Fact]
+    public void GetPhotoBase64FromFile_WhenFileHasInvalidExtension_ReturnsEmptyString()
+    {
+        var filePath = Path.Combine(TestFilesDirectory, InvalidExtensionFile);
+        File.WriteAllBytes(filePath, [0x01, 0x02, 0x03]);
 
-        [Fact]
-        public void GetPhotoBase64FromFile_WhenFileHasInvalidExtension_ReturnsEmptyString()
-        {
-            var filePath = Path.Combine(TestFilesDirectory, InvalidExtensionFile);
-            File.WriteAllBytes(filePath, [0x01, 0x02, 0x03]);
+        var result = PhotoUtils.GetPhotoBase64FromFile(filePath);
 
-            var result = PhotoUtils.GetPhotoBase64FromFile(filePath);
+        Assert.Equal(string.Empty, result);
+    }
 
-            Assert.Equal(string.Empty, result);
-        }
+    [Fact]
+    public void GetPhotoBase64FromFile_WhenValidJpgFile_ReturnsBase64String()
+    {
+        var filePath = Path.Combine(TestFilesDirectory, ValidJpgFile);
+        var testImageBytes = new byte[] { 0xFF, 0xD8, 0xFF, 0xE0, 0x00, 0x10, 0x4A, 0x46, 0x49, 0x46 };
+        File.WriteAllBytes(filePath, testImageBytes);
 
-        [Fact]
-        public void GetPhotoBase64FromFile_WhenValidJpgFile_ReturnsBase64String()
-        {
-            var filePath = Path.Combine(TestFilesDirectory, ValidJpgFile);
-            var testImageBytes = new byte[] { 0xFF, 0xD8, 0xFF, 0xE0, 0x00, 0x10, 0x4A, 0x46, 0x49, 0x46 };
-            File.WriteAllBytes(filePath, testImageBytes);
+        var result = PhotoUtils.GetPhotoBase64FromFile(filePath);
 
-            var result = PhotoUtils.GetPhotoBase64FromFile(filePath);
+        Assert.NotEqual(string.Empty, result);
+        Assert.Equal(Convert.ToBase64String(testImageBytes), result);
+    }
 
-            Assert.NotEqual(string.Empty, result);
-            Assert.Equal(Convert.ToBase64String(testImageBytes), result);
-        }
+    [Fact]
+    public void GetPhotoBase64FromFile_WhenValidJpegFile_ReturnsBase64String()
+    {
+        var filePath = Path.Combine(TestFilesDirectory, ValidJpegFile);
+        var testImageBytes = new byte[] { 0xFF, 0xD8, 0xFF, 0xE0, 0x00, 0x10, 0x4A, 0x46, 0x49, 0x46 };
+        File.WriteAllBytes(filePath, testImageBytes);
 
-        [Fact]
-        public void GetPhotoBase64FromFile_WhenValidJpegFile_ReturnsBase64String()
-        {
-            var filePath = Path.Combine(TestFilesDirectory, ValidJpegFile);
-            var testImageBytes = new byte[] { 0xFF, 0xD8, 0xFF, 0xE0, 0x00, 0x10, 0x4A, 0x46, 0x49, 0x46 };
-            File.WriteAllBytes(filePath, testImageBytes);
+        var result = PhotoUtils.GetPhotoBase64FromFile(filePath);
 
-            var result = PhotoUtils.GetPhotoBase64FromFile(filePath);
+        Assert.NotEqual(string.Empty, result);
+        Assert.Equal(Convert.ToBase64String(testImageBytes), result);
+    }
 
-            Assert.NotEqual(string.Empty, result);
-            Assert.Equal(Convert.ToBase64String(testImageBytes), result);
-        }
+    [Fact]
+    public void GetPhotoBase64FromFile_WhenEmptyJpgFile_ReturnsEmptyBase64String()
+    {
+        var filePath = Path.Combine(TestFilesDirectory, "empty.jpg");
+        File.WriteAllBytes(filePath, Array.Empty<byte>());
 
-        [Fact]
-        public void GetPhotoBase64FromFile_WhenEmptyJpgFile_ReturnsEmptyBase64String()
-        {
-            var filePath = Path.Combine(TestFilesDirectory, "empty.jpg");
-            File.WriteAllBytes(filePath, Array.Empty<byte>());
+        var result = PhotoUtils.GetPhotoBase64FromFile(filePath);
 
-            var result = PhotoUtils.GetPhotoBase64FromFile(filePath);
-
-            Assert.Equal(Convert.ToBase64String(Array.Empty<byte>()), result);
-        }
+        Assert.Equal(Convert.ToBase64String(Array.Empty<byte>()), result);
     }
 }
