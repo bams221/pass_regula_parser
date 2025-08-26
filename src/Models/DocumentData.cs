95c28bda628b5520ae3cf709c1ed8b40c4fdd6a3
samb1232 <xah300@mail.ru>
2025-08-26 15:51:59 +0500

    added default value to DataSaveAgreementDateEnd

--- src/Models/DocumentData.cs
+++ src/Models/DocumentData.cs
@@ -5,10 +5,15 @@ public abstract class DocumentData
     public string? DocumentType { get; set; }
     public string? Description { get; set; }
     public string DataSaveAgreement { get; set; } = "False";
-    public string DataSaveAgreementDateEnd { get; set; } = "30.01.2026";
+    public string DataSaveAgreementDateEnd { get; set; }
 
-     public override string ToString() =>
-    $@"Document Type: {DocumentType}
+    protected DocumentData()
+    {
+        DataSaveAgreementDateEnd = DateTime.Today.AddYears(1).ToString("dd.MM.yyyy");
+    }
+
+    public override string ToString() =>
+   $@"Document Type: {DocumentType}
     DataSaveAgreement: {DataSaveAgreement}
     DataSaveAgreementDateEnd: {DataSaveAgreementDateEnd}
     Description: {Description}";
